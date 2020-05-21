de7fcbf75f7be39929f85ba9a43a7b34d225a21d
Alehandro <alehandroxo18@gmail.com>
2020-05-21 18:47:48 +0300

    Lesson 1

---
+++ lesson_1.rb
@@ -0,0 +1,22 @@
+puts "Здравствуйте, как вас зовут?"
+name = gets.chomp
+
+puts "#{name}, какой у вас рост?"
+height = gets.chomp.to_i
+
+weight = "(#{height} - 110) * 1.15"
+
+if weight < 0
+    puts "{#{name} , ваш вес уже оптимальный"
+        
+    else weight >= 0
+        puts "#{name}, ваш идеальный вес : #{weight}"
+        
+    end
+
+end
+
+
+
+
+
